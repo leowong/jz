b9b9e6a9415be5f003fe878295a84714bac4b3c5
Leo Wong <gnowoel@gmail.com>
2011-02-16 03:19:59 +0800

    putting sessions in the database

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ Jzcrm::Application.config.session_store :cookie_store, :key => '_jzcrm_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Jzcrm::Application.config.session_store :active_record_store
+Jzcrm::Application.config.session_store :active_record_store
