97762acc60732cc39c1fc12288f3abb0fbb02ac7
Leo Wong <gnowoel@gmail.com>
2011-02-16 02:37:11 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jzcrm::Application.config.secret_token = 'e90fc70728b9c7b868e47e8f78dd0156a95401695154828a3ad507014210fd00a8a836e9ae9643c1ff126542579e177d97c56f172de3624c9600c34058e21c11'
