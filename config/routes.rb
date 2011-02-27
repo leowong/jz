5ad157f8aabe46709292078695b4afcdd684719d
Leo Wong <gnowoel@gmail.com>
2011-02-28 04:54:57 +0800

    generating scaffold for line item

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Jzcrm::Application.routes.draw do
+  resources :line_items
+
   resources :orders
 
   resources :contacts
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110227192158) do
+ActiveRecord::Schema.define(:version => 20110227195930) do
 
   create_table "contacts", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "order_id"
+    t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
