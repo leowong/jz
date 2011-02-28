b1a79b4c6d84bc4ca92eaed502145592ab63a701
Leo Wong <gnowoel@gmail.com>
2011-02-28 16:16:30 +0800

    setting contacts index as root page

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Jzcrm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "products#index"
+  root :to => "contacts#index"
 
   # See how all your routes lay out with "rake routes"
 
