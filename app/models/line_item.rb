1ef09c08d28d2296db0dc90ec37f0a8945781a06
Leo Wong <gnowoel@gmail.com>
2011-03-06 03:05:12 +0800

    adding validations for line item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,12 @@
 class LineItem < ActiveRecord::Base
+  attr_accessible :order, :product_id, :quantity
+  
   belongs_to :order
   belongs_to :product
+
+  validates :order_id, :product_id, :quantity, :presence => true
+  validates_associated :product
+  validates :quantity, :numericality => { :only_integer => true, :greater_than => 0 }
 
   def total_price
     product.price * quantity
