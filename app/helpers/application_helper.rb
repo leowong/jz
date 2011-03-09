a386e4a46596747e4d575a3cf5731d04373b0746
Leo Wong <gnowoel@gmail.com>
2011-03-10 05:53:58 +0800

    adding navigation tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def link_to_with_current_indicator(name, options = {}, html_options = {})
+    if current_page?(options)
+      html_options[:class] ? html_options[:class] += " current" : html_options[:class] += "current"
+    end
+    link_to(name, options, html_options)
+  end
 end
