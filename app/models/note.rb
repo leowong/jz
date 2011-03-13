22c45f79bee59c118a58938099348c06b955f5c9
Leo Wong <gnowoel@gmail.com>
2011-03-13 13:28:24 +0800

    showing notes for an order as activities

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,12 @@
 class Note < Annotation
+  def to_header
+    case self.annotatable.class.to_s 
+    when 'Contact' 
+      self.annotatable.name 
+    when 'Order'
+      self.annotatable.contact.name + " (##{annotatable.number})"
+    else
+      "Note"
+    end
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ActiveRecord::Base
   belongs_to :contact
   has_many :line_items, :dependent => :destroy
   has_many :notes, :as => :annotatable
+  has_many :activities, :as => :topic
 
   accepts_nested_attributes_for :line_items,
                                 :reject_if => proc { |attrs| attrs.any? { |k, v| v.blank? } },
