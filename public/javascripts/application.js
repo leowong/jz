06ea4418377be0d26ed5236a17e29ff613ca5be9
Leo Wong <gnowoel@gmail.com>
2011-03-18 08:04:43 +0800

    adding datepicker for selecting task due date

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -31,3 +31,8 @@ $(function() {
     return false;
   });
 });
+
+$(function() {
+  $.datepicker.setDefaults($.datepicker.regional["zh-CN"]);
+  $("#task_due_at").datepicker({dateFormat: "yy-mm-dd"});
+});
