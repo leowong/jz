29bbb97bf4034d620911cb85bd716b3e7ffa995e
Leo Wong <gnowoel@gmail.com>
2011-03-21 04:01:45 +0800

    adding mobile and phone columns to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # new columns need to be added here to be writable through mass assignment
-  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation, :mobile, :phone
 
   cattr_accessor :current_user
 
@@ -11,7 +11,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username
   validates_uniqueness_of :username, :email, :allow_blank => true
-  validates_format_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
+  validates_format_of :username, :with => /^[^\s]+$/i, :allow_blank => true, :message => "should not contain whitespace characters"
+  /\s/
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validates_presence_of :password, :on => :create
   validates_confirmation_of :password
