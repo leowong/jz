662a682849df0509650805a19e3821acbfb1e863
Leo Wong <gnowoel@gmail.com>
2011-03-22 00:52:34 +0800

    adding simple format for displaying text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
   def highlighting_billboard_tab?
     Billboard.all.size > 0 and Billboard.order("created_at").last.created_at.to_date == Time.zone.now.to_date ? true : false
   end
+
+  def wrap(content)
+    content = simple_format(h(content))
+  end
 end
