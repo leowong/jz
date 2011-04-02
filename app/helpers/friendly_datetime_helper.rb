4f62e504e700b87e5662c9a43aff3c0cbdd52c83
Leo Wong <gnowoel@gmail.com>
2011-04-02 09:22:55 +0800

    updating datetime format

--- app/helpers/friendly_datetime_helper.rb
+++ app/helpers/friendly_datetime_helper.rb
@@ -8,15 +8,15 @@ module FriendlyDatetimeHelper
       end
     elsif datetime >= Time.zone.now.beginning_of_year
       if opt[:style]
-        raw('<span class="timestamp this_year">' + datetime.strftime("%m-%d %H:%M") + '</span>')
+        raw('<span class="timestamp this_year">' + datetime.strftime("%m/%d %H:%M") + '</span>')
       else
-        datetime.strftime("%m-%d %H:%M")
+        datetime.strftime("%m/%d %H:%M")
       end
     else
       if opt[:style]
-        raw('<span class="timestamp before_this_year">' + datetime.strftime("%Y-%m-%d %H:%M") + '</span>')
+        raw('<span class="timestamp before_this_year">' + datetime.strftime("%Y/%m/%d %H:%M") + '</span>')
       else
-        datetime.strftime("%Y-%m-%d %H:%M")
+        datetime.strftime("%Y/%m/%d %H:%M")
       end
     end
   end
@@ -24,33 +24,33 @@ module FriendlyDatetimeHelper
   def friendly_date(datetime, opt={:style => false})
     if datetime.today?
       if opt[:style]
-        raw('<span class="timestamp today">' + datetime.strftime("%m-%d") + '</span>')
+        raw('<span class="timestamp today">' + datetime.strftime("%m/%d") + '</span>')
       else
-        datetime.strftime("%m-%d")
+        datetime.strftime("%m/%d")
       end
     elsif datetime >= Time.zone.now.beginning_of_year and datetime.to_date < Time.zone.now.to_date
       if opt[:style]
-        raw('<span class="timestamp this_year">' + datetime.strftime("%m-%d") + '</span>')
+        raw('<span class="timestamp this_year">' + datetime.strftime("%m/%d") + '</span>')
       else
-        datetime.strftime("%m-%d")
+        datetime.strftime("%m/%d")
       end
     elsif datetime < Time.zone.now.beginning_of_year
       if opt[:style]
-        raw('<span class="timestamp before_this_year">' + datetime.strftime("%Y-%m-%d") + '</span>')
+        raw('<span class="timestamp before_this_year">' + datetime.strftime("%Y/%m/%d") + '</span>')
       else
-        datetime.strftime("%Y-%m-%d")
+        datetime.strftime("%Y/%m/%d")
       end
     elsif datetime > Time.zone.now.to_date and datetime.to_date < Time.zone.now.next_year.beginning_of_year.to_date
       if opt[:style]
-        raw('<span class="timestamp this_year_but_beyond_today">' + datetime.strftime("%m-%d") + '</span>')
+        raw('<span class="timestamp this_year_but_beyond_today">' + datetime.strftime("%m/%d") + '</span>')
       else
-        datetime.strftime("%m-%d")
+        datetime.strftime("%m/%d")
       end
     else
       if opt[:style]
-        raw('<span class="timestamp">' + datetime.strftime("%Y-%m-%d") + '</span>')
+        raw('<span class="timestamp">' + datetime.strftime("%Y/%m/%d") + '</span>')
       else
-        datetime.strftime("%Y-%m-%d")
+        datetime.strftime("%Y/%m/%d")
       end
     end
   end
